3927c1c79fb5190843b7318c985c4fffb32723ff
KerstenSchaller <29780537*KerstenSchaller@users.noreply.github.com>
2018-06-24 14:56:14 +0200

    clean and simple...

--- WorkingTimeTracker/WorkTimeCalculator.cs
+++ WorkingTimeTracker/WorkTimeCalculator.cs
@@ -52,32 +52,19 @@ namespace WorkingTimeTracker
         public WorkTimeInfo get_current_day() { return current_day; }
 
         // triggers all actions related to a new activity
-        public string trigger_Activity()
+        public void trigger_Activity()
         {
             if ((DateTime.Now - last_active) > sample_time)
             {
-                return Analyze();
+                Analyze();
 
             }
-            return ""; 
-        }
-
-        //determines times for all days again
-        public void updateAllDays()
-        {
-
-            foreach (WorkTimeInfo wti in days)
-            {
-
-                if (!(current_day.special_flag ))
-                {
-                    determine_times(wti);
-                }
-            }
+             
         }
 
 
-        private string Analyze()
+
+        private void Analyze()
         {
             // Variables
             
@@ -118,235 +105,19 @@ namespace WorkingTimeTracker
                 current_day.addActivity(DateTime.Now);
                 last_active = current_day.last_active;
             }
-            if (days.Count > 1)
-            {
-                days = find_missing_days(days);
-            }
-
-
-
-            
-            current_day = determine_times(current_day);
-            
-
-            //save changes on days
-            Serialization.WriteToXmlFile<List<WorkTimeInfo>>(data_days_path, days);
-
-            string return_str = "start: "
-                              + current_day.getStartofWorkday().Hour.ToString()
-                              + ":"
-                              + current_day.getStartofWorkday().Minute.ToString()
-                              + " end: "
-                              + current_day.getEndofWorkday().Hour.ToString()
-                              + ":"
-                              + current_day.getEndofWorkday().Minute.ToString()
-                              + "  +/-: "
-                              + current_day.getPlusMinusTime().Hours.ToString()
-                              + "h:"
-                              + current_day.getPlusMinusTime().Minutes.ToString()
-                              + "m";
-            return return_str;
-
-
-
-
-        }
-
-        private List<WorkTimeInfo> find_missing_days(List<WorkTimeInfo> days)
-        {
-            WorkTimeInfo Day_before = days[0];
-            List<WorkTimeInfo> l = new List<WorkTimeInfo>();
-            l.Add(days[0]);
-            for (int i = 1;i<days.Count;i++)
-            {
-                //l.Add(days[i]);
-                //bool cond1 = days[i].date.Day != (Day_before.date.Day + 1);
-                //bool cond2 = !(Day_before.date.Day == 31 || Day_before.date.Day == 28 || Day_before.date.Day == 30) && (days[i].date.Day == 1);
-                DateTime  d = Day_before.date + new TimeSpan(24, 0, 0);
-                bool cond3 = !(days[i].date.Date.CompareTo((Day_before.date.Date + new TimeSpan(24, 0, 0))) == 0); 
-                if (cond3)
-                {
-                    WorkTimeInfo wti = new WorkTimeInfo();
-                    wti.date = Day_before.date + new TimeSpan(24,0,0);
-                    
-                    wti.special_flag = true;
-                    l.Add(wti);
-                    Day_before = wti;
-                    i--;
-                }
-                else
-                { 
-                    l.Add(days[i]);
-                    Day_before = days[i];
-                }
-            }
-            return l;
-        }
-
-        public TimeSpan getTotalPlusMinusTime()
-        {
-            TimeSpan t = new TimeSpan();
-            foreach (WorkTimeInfo wti in days)
-            {
-                if (wti != days.Last())
-                {
-                    t += wti.getPlusMinusTime();
-                }
        
-                
-            }
-            return t;
-        }
-
-
-
-        public WorkTimeInfo determine_times(WorkTimeInfo workday)
-        {
-            DateTime clock_time_5h = new DateTime(workday.date.Year, workday.date.Month, workday.date.Day, 5, 0, 0);
-            DateTime clock_time_20h = new DateTime(workday.date.Year, workday.date.Month, workday.date.Day, 20, 0, 0);
-            DateTime legal_start_time = clock_time_5h;
-            DateTime legal_end_time = clock_time_20h;
-
-
-
-            // get the activitys of the desired day
-            List<DateTime> activitys = workday.getActicitys();
-
-            // create a list of time periods
-            List<TimePeriod> timeperiods = new List<TimePeriod>();
-
-            // create a first timeperiod and set the first activitys time as start time
-            TimeSpan period_break_time = new TimeSpan();
-            timeperiods.Add(new TimePeriod());
-
-
-            if (((activitys.Count != 0) && (workday.special_flag == false))|| (workday.edit_flag == true))
-            {
-
-                if (activitys.Count != 0)
-                {
-                
-                    timeperiods[0].setStartTime(activitys[0]);
-            
-                    //loop through all activitys
-                    for (int i = 1; i < activitys.Count; i++)
-                    {
-                        period_break_time = activitys[i] - activitys[i-1];
-                        if ((period_break_time.TotalMinutes < max_period_break_time) && (activitys[i].CompareTo(legal_end_time) <=0 )) 
-                        {
-                            // if time between to activitys is below threshold, set the actual activity
-                            // time as new Endtime
-                            timeperiods.Last().setEndTime(activitys[i]);
-                
-                        }
-                        else
-                        {
-                            // if break exceeds threshold, create new time period with
-                            // start time from actual acitivity
-                            // if current activitys time is after legal end time
-                            // make legal end time the periods start time
-                            timeperiods.Add(new TimePeriod());
-
-                            if (activitys[i].CompareTo(legal_end_time) > 0)
-                            {
-                                timeperiods.Last().setStartTime(legal_end_time);
-                            }
-                            else
-                            {
-                                timeperiods.Last().setStartTime(activitys[i]);
-
-                            }
-
-                        }
-
-                
-                    }
-
-
-                    //sort out single activity periods
-                    List<TimePeriod> periods_temp = new List<TimePeriod>();
-                    foreach (TimePeriod t_p in timeperiods)
-                    {
-                        if (t_p.getNumberofActivities() > 1) periods_temp.Add(t_p);
-                    }
-
-                    timeperiods = periods_temp;
-
-
-                    // get start of day inside legal range
-                    foreach (TimePeriod t_p in timeperiods)
-                    {   
-
-                        if (t_p.getStartTime().CompareTo(legal_start_time) >= 0)
-                        {
-                            // if start time of period is equal or later to legal start time of workday
-                            // set start of workday and break execution of foreach
-                            workday.setStartofWorkday(t_p.getStartTime());
-                            break;
-                        }
-                    }
-
-                    // get end of day inside legal range
-                    for (int i = timeperiods.Count-1; i >= 0 ; i--  )
-                    {
-
-                        if (timeperiods[i].getEndTime().CompareTo(legal_end_time) <= 0)
-                        {
-                            // if start time of period is equal or earlier than legal end time of workday
-                            // set end of workday and break execution of foreach
-                            workday.setEndofWorkday(timeperiods[i].getEndTime());
-                            break;
-                        }
-                    }
-
-                }
-
-                // determine plus minus time for each day
-                if (workday.getStartofWorkday().Year != 0001)
-                {
-                    TimeSpan legal_working_time = new TimeSpan(8, 24, 0);
-                    TimeSpan hours_worked = workday.getEndofWorkday() - workday.getStartofWorkday();
-                    TimeSpan pause = new TimeSpan(0, 0, 0);
-                    if (hours_worked > new TimeSpan(6, 0, 0)) // substract pause after 6 hours
-                    {
-                        pause += new TimeSpan(0, 30, 0);
-                    }
-                    if (hours_worked > new TimeSpan(9, 0, 0)) // substract pause after 9 hours
-                    {
-                        pause += new TimeSpan(0, 15, 0);
-                    }
-                    pause += workday.get_individual_break();
-                    workday.set_total_break(pause);
-                    hours_worked -= pause;
-                    workday.set_worktime( hours_worked);
-                    TimeSpan plusminus = hours_worked - legal_working_time;
-                    workday.setPlusMinusTime(plusminus);
-
-                }
-                else
-                {
-                    workday.setPlusMinusTime(new TimeSpan(0, 0, 0));
-                }
-            }
-
-            if ((workday.special_flag == true) && (workday.sick_flag == false) && (workday.vacation_flag == false))
-            {
-                return workday;
-            }
-            if ((workday.sick_flag == true) ||(workday.vacation_flag == true))
-            {
-                // set start and end of workday to default values
-                DateTime d = DateTime.Now;
-                workday.setStartofWorkday(new DateTime(d.Year, d.Month, d.Day, 8, 0, 0));
-                workday.setEndofWorkday(new DateTime(d.Year, d.Month, d.Day, 8 + workday.get_legal_worktime().Hours, workday.get_legal_worktime().Minutes + 30/*Pause*/, 0));
-                workday.setPlusMinusTime(new TimeSpan(0,0,0));
-                
-            }
-            return workday;
-
-
 
         }
+
+        
+
+
+
+
+        
+
+
+        
 
 
     }
--- WorkingTimeTracker/WorkTimeInfo.cs
+++ WorkingTimeTracker/WorkTimeInfo.cs
@@ -15,40 +15,14 @@ namespace WorkingTimeTracker
         public DateTime last_active = new DateTime();
         public DateTime first_active = new DateTime();
         public List<DateTime> activitys = new List<DateTime>();
-        public String plus_minus_time = "00:00:00";
-        public bool edit_flag = false;
-        public bool special_flag = false;
-        public bool sick_flag = false;
-        public bool vacation_flag = false;
-        public String individual_break = "00:00:00";
-        public String total_break = "00:00:00";
-        public String legal_worktime = "00:00:00";
-        public String worktime = "00:00:00";
 
-
-      public WorkTimeInfo()
+        public WorkTimeInfo()
         {
             date = DateTime.Now;
-            first_active = date;
-            last_active = date;
-            legal_worktime = convertTimeSpantoString(new TimeSpan(8,24,0));
-
-
+            
         }
 
-        public void set_legal_worktime(TimeSpan t){legal_worktime = convertTimeSpantoString(t); }
-        public TimeSpan  get_legal_worktime() {return convertStringtoTimeSpan(legal_worktime); }
-
-        public void set_worktime(TimeSpan t) { worktime = convertTimeSpantoString(t); }
-        public TimeSpan get_worktime() { return convertStringtoTimeSpan(worktime); }
-
-
-      public TimeSpan getPlusMinusTime(){return convertStringtoTimeSpan( plus_minus_time); }
-        public void setPlusMinusTime(TimeSpan time) { plus_minus_time = convertTimeSpantoString( time); }
-
-        public void set_total_break(TimeSpan t) { total_break = convertTimeSpantoString(t); }
-        public TimeSpan get_total_break() { return convertStringtoTimeSpan(total_break); }
-
+      
 
         public void setStartofWorkday(DateTime time) { start_of_workday = time; }
         public DateTime getStartofWorkday() { return start_of_workday; }
@@ -72,11 +46,8 @@ namespace WorkingTimeTracker
         }
 
         
-        public TimeSpan getWorkingHours()
-        {
-            //return (this.end_of_workday - this.start_of_workday);
-            return this.get_worktime();
-        }
+      
+
 
 
 
@@ -114,15 +85,6 @@ namespace WorkingTimeTracker
             return dummy;
         }
 
-        public TimeSpan get_individual_break()
-        {
-            return convertStringtoTimeSpan( this.individual_break);
-        }
-
-        public void set_individual_break(TimeSpan time)
-        {
-             this.individual_break =  convertTimeSpantoString( time);
-        }
 
         public String convertTimeSpantoString(TimeSpan t)
         {
@@ -132,13 +94,13 @@ namespace WorkingTimeTracker
         public TimeSpan convertStringtoTimeSpan(string s)
         {
 
-               String[] times = s.Split(':');
+            String[] times = s.Split(':');
             if (times[2].Contains("."))
             {
                 String[] t = times[2].Split('.');
                 times[2] = t[0];
             }
-               return new TimeSpan(Int32.Parse(times[0]), Int32.Parse(times[1]), Int32.Parse (times[2]));
+            return new TimeSpan(Int32.Parse(times[0]), Int32.Parse(times[1]), Int32.Parse (times[2]));
         }
     }
 }
